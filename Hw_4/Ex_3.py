ec5db3c0d5871a2d479fe7e5e04d8046cd3dacc9
Evgeniya <sluchevskaya0309@yandex.ru>
2022-10-14 14:42:16 +0300

    add HW_4

--- Hw_4/Ex_3.py
+++ Hw_4/Ex_3.py
@@ -17,6 +17,28 @@
 # out
 # [4, 4, 5, 5, 6, 2, 3, 0, 9, 4]
 # [6, 2, 3, 0, 9]
+from itertools import count
+from random import randrange
+num = int(input('Введите количество чисел: '))
+def numbers(num: int):
+    num_list = []
+    if num<0:
+        print('Incorrectly number')
+        return []
+    for i in range(1, num+1):
+        num_list.append(randrange(0, num+1))
+    return num_list    
+
+    
+
+def origin_nums(num_list: list):
+    end_list = []
+    for i in num_list:
+          if num_list.count(i)==1:
+            end_list.append(i)
+    return end_list 
+n_list = numbers(num)    
+print(n_list, origin_nums(n_list), sep='\n')
 
 
 
